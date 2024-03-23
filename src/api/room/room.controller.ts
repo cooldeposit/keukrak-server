8239c7b2d68f83caadc765412c16265ea06957f9
r4bb1t <r4bb1t.github@gmail.com>
2024-03-24 07:06:31 +0900

    ai speed

--- src/api/room/room.controller.ts
+++ src/api/room/room.controller.ts
@@ -202,7 +202,7 @@ export const nextQuestion = async (ctx: Context) => {
       },
     ];
     AppDataSource.getRepository(Room).save(room);
-  }, (room.currentQuestion === 0 ? 15000 : 8000) + Math.random() * 1000 + r.length * 500);
+  }, (room.currentQuestion === 0 ? 10000 : 4000) + Math.random() * 1000 + r.length * 500);
 };
 
 export const poll = async (ctx: Context) => {
