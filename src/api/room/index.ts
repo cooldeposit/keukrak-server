ff2bc2797638383a81fc39c5dc2bcd27641fb1b7
r4bb1t <r4bb1t.github@gmail.com>
2024-03-24 09:22:29 +0900

    feat: poll

--- src/api/room/index.ts
+++ src/api/room/index.ts
@@ -8,5 +8,6 @@ roomRouter.get("/:roomId", Controller.getRoom);
 roomRouter.post("/:roomId", Controller.changeConnect);
 roomRouter.post("/:roomId/enter", Controller.enterRoom);
 roomRouter.get("/:roomId/next", Controller.nextQuestion);
+roomRouter.post("/:roomId/poll", Controller.poll);
 
 export default roomRouter;
